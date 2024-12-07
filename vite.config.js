449d449d831e3f0697c4e1889c71bdfab29d82f8
425rohan <rohanmore425@gmail.com>
2024-12-07 13:45:35 +0530

    removed custom plugin from vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base : "/425rohan/Todo-App-React/",
   plugins: [
-    react(),
-    {
-      name: 'html-transform',
-      enforce: 'post',
-      apply: 'build',
-      generateBundle(_, bundle) {
-        for (const file in bundle) {
-          if (file.endsWith('.html')) {
-            const html = bundle[file].source.toString()
-            const transformedHtml = html.replace(
-              /<script type="module" src="\/425rohan\/Todo-App-React\/index.js"><\/script>/,
-              '</body><script type="module" src="/425rohan/Todo-App-React/index.js"></script>'
-            )
-            bundle[file].source = transformedHtml.replace('</body>', '<script type="module" src="/425rohan/Todo-App-React/index.js"></script></body>')
-          }
-        }
-      }
-    }
+    react()
   ]
 
 })
